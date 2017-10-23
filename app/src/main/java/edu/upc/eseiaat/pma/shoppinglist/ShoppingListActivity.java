58ba8dd319be6e1c80624924d142ce2d4490c54d
juliaromeu <juliaromeu03@gmail.com>
2017-10-23 12:09:23 +0200

    Activar btn_add Evitar cuadros en blanco

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,66 @@ package edu.upc.eseiaat.pma.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ShoppingListActivity extends AppCompatActivity {
+    //Lo que necesita una lista
+    private ArrayList<String> itemList;
+    private ArrayAdapter<String> adapter;
+
+
+    private ListView list;
+    private Button btn_add;
+    private EditText edit_item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
+
+        //CREAR CAMPOS: no las quiero como variables locales
+        //ANTES: ListView list = (ListView) findViewById(R.id.list)
+        //DESPUÉS: list = (ListView) findViewById(R.id.list);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        edit_item = (EditText) findViewById(R.id.edit_item);
+
+        //CONSTRUIR OBJETO
+        itemList = new ArrayList<>();
+        itemList.add("Patatas");
+        itemList.add("Zanahorias");
+        itemList.add("Papel WC");
+        itemList.add("Copas Danone");
+
+        //CREAR ADAPTADOR
+        //this=puntero a la actividad actual, hace referencia a ella
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemList);
+
+        //LISTENER DE CUANDO CLICAN EL BOTÓN
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();
+            }
+        });
+
+        //Se necesita ponerlo siempre en una lista, lo añadimos al inicio del proyecto
+        list.setAdapter(adapter);
+    }
+
+    private void addItem() {
+        String item_text= edit_item.getText().toString();
+        //Evitar cuadros en blanco. Añadir solo si hay algo escrito.
+        if (!item_text.isEmpty()){
+        itemList.add(item_text);
+        adapter.notifyDataSetChanged();
+        }
     }
 }
